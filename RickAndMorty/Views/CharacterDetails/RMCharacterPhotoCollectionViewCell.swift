f7f8b7ff1bbf59426a9cb3de6a3c08f418a83b67
Egor Moroz <egormoroz@Egors-MacBook-Air.local>
2024-09-09 22:09:24 +0300

    Added image in detail view

--- RickAndMorty/Views/CharacterDetails/RMCharacterPhotoCollectionViewCell.swift
+++ RickAndMorty/Views/CharacterDetails/RMCharacterPhotoCollectionViewCell.swift
@@ -10,24 +10,48 @@ import UIKit
 final class RMCharacterPhotoCollectionViewCell: UICollectionViewCell {
     static let cellIdentifier = "RMCharacterPhotoCollectionViewCell"
     
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+        contentView.addSubview(imageView)
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.rightAnchor.constraint(equalTo: contentView.rightAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            imageView.leftAnchor.constraint(equalTo: contentView.leftAnchor)
+        ])
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func setupConstraints() {
-        
-    }
-    
     override func prepareForReuse() {
         super.prepareForReuse()
+        imageView.image = nil
     }
     
     public func configure(with viewModel: RMCharacterPhotoCollectionViewCellViewModel) {
-        
+        viewModel.fetchImage { [weak self] result in
+            switch result {
+            case .success(let success):
+                DispatchQueue.main.async {
+                    self?.imageView.image = UIImage(data: success)
+                }
+            case .failure(let failure):
+                break
+            }
+        }
     }
 }
